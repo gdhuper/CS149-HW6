b3c27c9e9b93d4eda216572c4e8f46f3df1d3147
Gurpreet Singh <gdhuper@gmail.com>
2016-11-27 00:21:26 -0800

    timestamp works: sorta

--- main.c
+++ main.c
@@ -14,44 +14,38 @@
 struct timeval start;
 
 
- /*
-  * Calculates elapsed time
-  */
- double getElapsedSeconds()
- {
-     struct timeval now;
-     gettimeofday(&now, NULL);
-     double seconds = (double) now.tv_sec - start.tv_sec;
-     double useconds = (double) now.tv_usec - start.tv_usec;
-     double result = seconds + (useconds / 1000000);
-     return round(result * 1000.0) / 1000.0;
- }
-
-/**
- * formats current time
+/*
+ * Calculates elapsed time and formats it
  */
-void currentMinSec(int* minVal, double* secVal)
+void getElapsedSeconds(int* minsElapsed, double* secsElapsed)
 {
-    double timeElapsed = getElapsedSeconds();
-    int intSec = floor(timeElapsed);
-    *secVal = timeElapsed - intSec;
-    *minVal = intSec / 60;
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    double seconds = (double) now.tv_sec - start.tv_sec;
+    double useconds = (double) now.tv_usec - start.tv_usec;
+    double result = seconds + (useconds / 1000000);
+    double temp =  round(result * 1000.0) / 1000.0;
+
+    int intSec = floor(temp);
+    *secsElapsed = temp - intSec;
+    *minsElapsed = intSec / 60;
     intSec %= 60;
-    *secVal += intSec;
+    *secsElapsed += intSec;
+
 }
+
+
 
 
 int main() {
 
-    int minVal;
-    double secVal;
+    int minsElapsed;
+    double secsElapsed;
     for (int i = 0; i < 10; ++i) {
-
-         printf("%lf", getElapsedSeconds());
+        getElapsedSeconds(&minsElapsed, &secsElapsed);
+        printf("%1d:%2.3f", 0, secsElapsed);
         printf("%\n");
-        sleep(1);
-       // currentMinSec(&minVal, &secVal);
-        //printf("\n%d:%06.3f: Parent received message:\n", minVal, secVal);
+        sleep(1); //for testing generating time stamps
     }
     return 0;
 }
