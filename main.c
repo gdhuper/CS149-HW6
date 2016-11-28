851f13ce7d43db322fc4d7a59b632aec737f9d32
JaylanT <Jaylan.Tse@gmail.com>
2016-11-27 18:32:47 -0800

    create 5 pipes and child processes

--- main.c
+++ main.c
@@ -10,6 +10,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <string.h>
+
+#define BUFFER_SIZE 32
+#define READ_END 0
+#define WRITE_END 1
+#define NUM_PIPES 5
+#define NUM_CHILD 5
 
 struct timeval start;
 
@@ -38,14 +45,49 @@ void getElapsedSeconds(int* minsElapsed, double* secsElapsed)
 
 
 int main() {
+	char write_msg[BUFFER_SIZE] = "You're my child process";
+	char read_msg[BUFFER_SIZE];
 
+	pid_t pid;
+	int fd[2 * NUM_PIPES];
+	int i, j;
     int minsElapsed;
     double secsElapsed;
-    for (int i = 0; i < 10; ++i) {
-        getElapsedSeconds(&minsElapsed, &secsElapsed);
-        printf("%1d:%2.3f", 0, secsElapsed);
-        printf("%\n");
-        sleep(1); //for testing generating time stamps
-    }
+
+	for (i = 0; i < NUM_PIPES; i += 2) {
+		if (pipe(fd + i) == -1) {
+			fprintf(stderr, "pipe() failed");	
+			return 1;
+		}
+	}
+
+	for (i = 0, j = 2 * i; i < NUM_CHILD; i++) {
+		pid = fork();
+		if (pid > 0) {
+			close(fd[READ_END + j]);
+			write(fd[WRITE_END + j], write_msg, strlen(write_msg) + 1);
+
+			printf("Parent: Wrote '%s' to the pipe.\n", write_msg);
+
+			close(fd[WRITE_END + j]);
+		} else if (pid == 0) {
+			close(fd[WRITE_END + j]);
+			read(fd[READ_END + j], read_msg, BUFFER_SIZE);
+
+			printf("Child: Read '%s' from the pipe.\n", read_msg);
+
+			close(fd[READ_END + j]);
+		} else {
+			fprintf(stderr, "fork() failed");
+			return 1;
+		}
+	}
+
+//    for (int i = 0; i < 10; ++i) {
+//        getElapsedSeconds(&minsElapsed, &secsElapsed);
+//        printf("%1d:%2.3f", 0, secsElapsed);
+//        printf("\n");
+//        sleep(1); //for testing generating time stamps
+//    }
     return 0;
 }
