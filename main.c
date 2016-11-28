f1a4b1bb7102168f1b3d7eee0c085fd94f109c27
JaylanT <Jaylan.Tse@gmail.com>
2016-11-28 01:09:21 -0800

    use select() to read messages

--- main.c
+++ main.c
@@ -11,7 +11,8 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <string.h>
-#include <pthread.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
 
 #define BUFFER_SIZE 256
 #define READ_END 0
@@ -21,7 +22,6 @@
 #define MAX_TIME 30.0
 
 struct timeval start;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 /*
  * Calculates elapsed time and formats it
@@ -44,27 +44,32 @@ void getElapsedSeconds(int* minsElapsed, double* secsElapsed)
 }
 
 int main() {
-	char write_msg[BUFFER_SIZE];
 	char read_msg[BUFFER_SIZE];
 
 	time_t start, end;
 	double elapsed;
-	int terminate = 1;
 	pid_t pid;
 	int fd[NUM_PIPES * 2];
 	int i, j, k, l;
 	int minsElapsed;
 	double secsElapsed;
 
-	//srand(time(NULL));
+	char buffer[128];
+	int result, nread;
+
+	fd_set inputs, inputfds;
+	struct timeval timeout;
+
+	FD_ZERO(&inputs);
 
 	for (i = 0; i < NUM_PIPES * 2; i += 2) {
 		if (pipe(fd + i) == -1) {
 			fprintf(stderr, "pipe() failed");	
 			return 1;
 		}
+		FD_SET(fd[i], &inputs);
 	}
-	
+
 	// Create child processes
 	for (i = 0; i < NUM_CHILD; ++i) {
 		pid = fork();	
@@ -79,38 +84,64 @@ int main() {
 			return 1;
 		}
 	}
-	
+
 	if (pid > 0) {
 		// parent process
-		
+
 		k = NUM_CHILD;
 		start = time(NULL);
 
 		while (k > 0) {
 			for (j = 0; j < NUM_CHILD * 2; j += 2) {
-				// need to use select
-				read(fd[READ_END + j], read_msg, BUFFER_SIZE);
+				inputfds = inputs;
 
-				if (strcmp(read_msg, "0") == 0) {
-					--k;
-					close(fd[READ_END + j]);
-				} else {
-					puts(read_msg);
+				timeout.tv_sec = 2;
+				timeout.tv_usec = 500000;
+
+				result = select(fd[READ_END + j] + 1, &inputfds,
+						(fd_set *) 0, (fd_set *) 0, &timeout);
+
+				switch(result) {
+					case 0: {
+						// child terminated
+						--k;
+						fflush(stdout);
+						break;
+					}
+					case -1: {
+						perror("select");
+						exit(1);
+					}
+					default: {
+						if (FD_ISSET(fd[READ_END + j], &inputfds)) {
+							ioctl(fd[READ_END + j], FIONREAD, &nread);
+
+							if (nread == 0) {
+								printf("Keyboard input done.\n");
+								exit(0);
+							}
+
+							nread = read(fd[READ_END + j], buffer, nread);
+							buffer[nread] = 0;
+							printf("%s\n", buffer);
+						}			 
+					}
 				}
 			}
 		}
 		puts("All children exited");
 	} else if (pid == 0) {
 		// child process
-		
+
+		char buf[BUFFER_SIZE];
+
 		srand(time(NULL) ^ (getpid()<<16));
 		start = time(NULL);
 		elapsed = 0;
 		l = 1;
-		
+
 		// do stuff for 30 seconds
 		while (elapsed < MAX_TIME) {
-			char buf[BUFFER_SIZE];
 			snprintf(buf, sizeof buf, "%2.3f: Child %d message %d", elapsed, i + 1, l++);
 
 			write(fd[WRITE_END + j], buf, strlen(buf) + 1);
@@ -119,8 +150,7 @@ int main() {
 			end = time(NULL);
 			elapsed = difftime(end, start);
 		}
-		printf("Child done at %f\n", elapsed);
-		write(fd[WRITE_END + j], "0", strlen("0") + 1);
+		printf("Child %d done at %f\n", i + 1, elapsed);
 		close(fd[WRITE_END + j]);
 		exit(0);
 	}
